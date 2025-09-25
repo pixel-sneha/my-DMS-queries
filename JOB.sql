e37d7aedb183fd999203d6ae305a2dda0f578a86
Sne <sweetsonu2006in@gmail.com>
2025-09-25 20:07:59 +0530

    Create JOB.sql

---
+++ JOB.sql
@@ -0,0 +1,8 @@
+Create Table Job (job_id Varchar2(15), job_title Varchar2(30), min_sal NUMBER(7,2), max_sal NUMBER(7,2));
+
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('IT_PROG','Programmer',4000,10000);
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('MK_MGR','Marketing manager',9000,15000);
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('FI_MGR','Finance Manager',8200,12000);
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('FI_ACC','Account',4200,9000);
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('LEC','Lecture',6000,17000);
+INSERT INTO Job (job_id, job_title, min_sal, max_sal) VALUES('COMP_OP','Computer Operator',1500,3000);

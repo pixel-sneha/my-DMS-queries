e11d239626433357bc9713e63b6391a04e2a2f7d
Sne <sweetsonu2006in@gmail.com>
2025-11-02 15:28:36 +0530

    Update mydms26.sql

--- mydms26.sql
+++ mydms26.sql
@@ -14,3 +14,11 @@ where (current_year - expiration_year>6)
 or (expiration_year - current_year = 1) 
 or (expiration_year - current_year = 0)
 order by expiration_year-current_year;
+
+--A quartic function is defined: f(x) = ax4+bx3+cx2+dx+e
+--constant parameters:a = 3,b = 5,c = 0.9,d = 2.2,e = 1
+--Fetch the ids and the quartic function where x is the id, and rename the column to quartic
+--without using built in functions like power
+select id, 3*(id*id*id*id)+5*(id*id*id)+0.9*(id*id)+2.2*(id)+1
+as quartic
+from items;

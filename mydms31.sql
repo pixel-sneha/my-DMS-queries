36d5310fd757adc21322d16f12d267d05bfeccc7
Sne <sweetsonu2006in@gmail.com>
2025-11-07 18:53:09 +0530

    sub-query exercise
    Added a query to find categories with total quantities above the average.

--- mydms31.sql
+++ mydms31.sql
@@ -4,3 +4,10 @@
 select * 
 from shop
 where price > (select avg(price) from shop);
+
+--Find the average of ALL quantities (This is the subquery)
+--for each category, sum their quantities and compare with this average (use the HAVING keyword for group by)
+select category, sum(quantity) as total_quantity 
+from shop
+group by category
+having sum(quantity) > (select avg(quantity) from shop);

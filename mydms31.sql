12e98d4e6e86a4426746d6e139b14c0b69d136a1
Sne <sweetsonu2006in@gmail.com>
2025-11-08 20:15:40 +0530

    Update mydms31.sql

--- mydms31.sql
+++ mydms31.sql
@@ -11,3 +11,17 @@ select category, sum(quantity) as total_quantity
 from shop
 group by category
 having sum(quantity) > (select avg(quantity) from shop);
+
+--calculate the average price across all items within the specified date range (2015-01-01  to 2015-03-18) 
+--Add this average price to each item's original price to get the correct price value --For each category,
+--calculate the total revenue by multiplying the corrected price by the quantity and summing these values 
+--Present the results as (category, total_revenue) pairs, sorted by total revenue in descending order
+
+SELECT category, SUM(price * quantity) AS total_revenue
+FROM (
+    SELECT price + (SELECT AVG(price) FROM shop WHERE list_date BETWEEN '2015-01-01' AND '2015-03-18') AS price, quantity, category, list_date
+    FROM shop
+    WHERE list_date BETWEEN '2015-01-01' AND '2015-03-18'
+)
+GROUP BY category
+ORDER BY total_revenue DESC

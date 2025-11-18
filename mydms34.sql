bf354082fbad543f9dd50d6ec04871b000e58b43
Sne <sweetsonu2006in@gmail.com>
2025-11-18 22:00:11 +0530

    Update mydms34.sql

--- mydms34.sql
+++ mydms34.sql
@@ -8,3 +8,12 @@ union all select product_id, quantity_sold from sales_2011) as combined_sales
 group by product_id
 order by total_sales desc;
 
+--write a query that First identifies "large devices" (devices with width greater than 200)
+--Then calculates the average score for these large devices. Call this column as average_score
+with large_devices as (
+    select * from devices_specs 
+    join devices_score on devices_score.device_id = devices_specs.device_id
+    where width > 200
+)
+select avg(score) as average_score 
+from large_devices;

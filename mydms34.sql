15ada772dbede3544b0990ee1a88b0f9a3299425
Sne <sweetsonu2006in@gmail.com>
2025-11-21 18:56:23 +0530

    Update mydms34.sql

--- mydms34.sql
+++ mydms34.sql
@@ -32,3 +32,26 @@ JOIN devices_score dsc
 WHERE ds.opinion > (SELECT avg_q FROM avg_quality)
   AND dsc.score   < (SELECT avg_q FROM avg_quality);
 
+--Some analysts forecast what is going to be the growth for real estate assets. We want to check if their forecast is true. 
+--For each asset in the real_estate table, calculate the ratio between the value of a newer date to the value of an older date.
+--For example, if there is an asset whose value in 2022-01-01 was 5 and in 2022-05-01 was 10 then that ratio is 10/5 = 2.
+--Ignore all of the comparisons that the day difference between the two dates is smaller than 101.
+--There are many dates for each asset so there are many ratios to compare. keep only the biggest ratio for each asset.
+--divide this ratio with the matching forecast from the potential table (call this field ratio_forecast) and sort the results in descending order.
+--The final result should have the asset_id and ratio_forecast.
+--Keep only the assets where the ratio_forecast is bigger than 1.
+
+with max_ratios as (
+    select r1.asset_id,max((r2.value * 1.0)/r1.value) as max_ratio
+from real_estate r1
+join real_estate r2 
+on r1.asset_id = r2.asset_id and r1.date < r2.date
+where julianday(r2.date) - julianday(r1.date) > 100
+group by r1.asset_id
+)
+select maxr.asset_id, (maxr.max_ratio/pt.forecast) as ratio_forecast
+from max_ratios maxr
+join potential pt 
+on maxr.asset_id = pt.asset_id
+where (maxr.max_ratio/pt.forecast) > 1
+order by ratio_forecast desc;
